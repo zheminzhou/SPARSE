f4c55769646132b572a8ccb8b97cb04913d2c16e
Nabil-Fareed Alikhan <nabil@happykhan.com>
2017-11-07 12:11:02 +0000

    Mock modules

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,17 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import sys
+
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return MagicMock()
+
+MOCK_MODULES = ['pycapnp']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
 
